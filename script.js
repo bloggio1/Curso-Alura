222305025ee17a7b2b649bdfb6e44e0aee5c9f1f
bloggio1 <124635563+bloggio1@users.noreply.github.com>
2023-04-12 01:05:09 -0500

    Reemplazar caracteres especiales por un espacio

--- script.js
+++ script.js
@@ -4,17 +4,25 @@ let encriptador = {
     "a": "ai",
     "o": "ober",
     "u": "ufat"
+    
 };
+function mayuscu() {
+    alert("No se permiten mayúsculas en el texto. Por favor, introduce sólo minúsculas.");
+    return;
+}
 
 function encriptar() {
     let texto = document.getElementById("texto").value;
     if (texto !== "") {
+        if (/[A-Z]/.test(texto)) {
+            mayuscu();
+        }
     let textoEncriptado = "";
     for (let i = 0; i < texto.length; i++) {
         const letra = texto[i];
         textoEncriptado += encriptador[letra] || letra;
     }
-    document.getElementById("resultado2").innerHTML = textoEncriptado;
+    document.getElementById("resultado2").innerHTML = textoEncriptado.replace(/[^a-z]/gi, ' ');
     document.getElementById("copiardiv").style.display = "block";
 
 }else{
@@ -25,8 +33,11 @@ function encriptar() {
 function desencriptar() {
     let textoDes = document.getElementById("texto").value;
     if (textoDes !== "") {
-    let textoDesencriptado = textoDes.replace(/enter/g, 'e').replace(/imes/g, 'i').replace(/ai/g, 'a').replace(/ober/g, 'o').replace(/ufat/g, 'u');
-    document.getElementById("resultado2").innerHTML = textoDesencriptado;
+        if (/[A-Z]/.test(textoDes)) {
+            mayuscu();
+        }
+    let textoDesencriptado = textoDes.replace(/enter/g, 'e').replace(/imes/g, 'i').replace(/ai/g, 'a').replace(/ober/g, 'o').replace(/ufat/g, 'u').replace(/ufat/g, 'u');
+    document.getElementById("resultado2").innerHTML = textoDesencriptado.replace(/[^a-z]/gi, ' ');
     document.getElementById("copiardiv").style.display = "block";
     
 }else{
